dd773d3a81d305e6ab260b7bb3334e20f0ce9db4
ehsanjaliilifar <ehsan.jallifar@gmail.com>
2023-02-15 10:39:35 -0600

    Obstacle locator script added

---
+++ TreasureHunter/Assets/UserDefined/Scripts/ObstacleLocator.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleLocator : MonoBehaviour
+{
+    [SerializeField] private GameObject ground;
+
+    float xRange, zRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        xRange = ground.transform.localScale.x / 2.0f;
+        zRange = ground.transform.localScale.z / 2.0f;
+
+        AssignRandomPosition();
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Treasures should be reachable. So, relocate them if they are located inside an obstacle.
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            AssignRandomPosition();
+            Debug.Log("Obstacle was relocated");
+        }
+
+    }
+
+    private void AssignRandomPosition()
+    {
+        transform.position = new Vector3(
+                Random.Range(-xRange + 1f, xRange - 1f),
+                transform.position.y,
+                Random.Range(-zRange + 1f, zRange - 1f));
+    }
+}

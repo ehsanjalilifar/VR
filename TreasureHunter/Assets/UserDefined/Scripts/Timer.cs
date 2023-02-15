d73040d10ee46d3a7485d2efe90d8745662a3526
ehsanjaliilifar <ehsan.jallifar@gmail.com>
2023-02-15 13:07:54 -0600

    Basic UI with elapsed time

---
+++ TreasureHunter/Assets/UserDefined/Scripts/Timer.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI gameTime;
+
+    private float seconds, minutes, startTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float ellapsedTime = Time.time - startTime;
+        minutes = (int)(ellapsedTime / 60f);
+        seconds = (int)(ellapsedTime % 60f);
+        gameTime.text = "Ellapced Time  " + minutes.ToString("00") + ":" + seconds.ToString("00");
+    }
+}
